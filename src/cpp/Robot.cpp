17842bc2b8e71cb8f1f2412c581eb463f7e2773b
Zechariah Hounshel <monkeyinastripedshirt@gmail.com>
2018-03-08 00:45:45 -0500

    Initial Commit

---
+++ src/cpp/Robot.cpp
@@ -0,0 +1,18 @@
+class Robot: public IterativeRobot {
+public:
+    Robot() { }
+
+    void RobotInit() { }
+
+    void DisabledInit() { }
+    void AutonomousInit() { }
+    void TeleopInit() { }
+    void TestInit() { }
+
+    void DisabledPeriodic() { }
+    void AutonomousPeriodic() { }
+    void TeleopPeriodic() { }
+    void TestPeriodic() { }
+};
+
+START_ROBOT_CLASS(Robot)
